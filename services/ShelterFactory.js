39c47fb6577bc9c8118a4acdaa6bacacaeccb9d7
Connor Abdelnoor & Reid Baldwin <student@epicodus.com>
2015-04-21 15:57:30 -0700

    add icon functionality

--- services/ShelterFactory.js
+++ services/ShelterFactory.js
@@ -1,7 +1,7 @@
 animalShelter.factory('ShelterFactory', function ShelterFactory() {
     var factory = {};
     factory.animals = [];
-    
+
     factory.addAnimal = function(name, type) {
         this.animals.push({ name:name, type:type, adopted:false })
     };
