bd176ec3c41978e8499f0a218db6ed0a2c7fd5fc
Connor Abdelnoor & Reid Baldwin <student@epicodus.com>
2015-04-21 14:56:39 -0700

    add super dope drop down type selector

--- controllers/AnimalController.js
+++ controllers/AnimalController.js
@@ -1,9 +1,15 @@
-animalShelter.controller('AnimalsCtrl', function AnimalsCtrl($scope, AnimalsFactory) {
+animalShelter.controller('AnimalsCtrl', function AnimalsCtrl($scope, ShelterFactory) {
 
-    $scope.animals = AnimalsFactory.animals;
-    console.log($scope.animals);
+    $scope.animals = ShelterFactory.animals;
+
+    $scope.typeOptions = ['Dog', 'Cat', 'Bunny', 'Turtle'];
+
+    $scope.animalType = $scope.typeOptions[0];
 
     $scope.addAnimal = function() {
-
-    }
+        var name = $scope.animalName;
+        var type = $scope.animalType;
+        ShelterFactory.addAnimal(name, type);
+        $scope.animalName = null;
+    };
 })
--- services/ShelterFactory.js
+++ services/ShelterFactory.js
@@ -1,5 +1,8 @@
-animalShelter.factory('AnimalsFactory', function AnimalsFactory() {
+animalShelter.factory('ShelterFactory', function ShelterFactory() {
     var factory = {};
-    factory.animals = [ { name: "Fido", type: "rabbit", adopted: false }];
+    factory.animals = [];
+    factory.addAnimal = function(name, type) {
+        this.animals.push({ name:name, type:type, adopted:false })
+    }
         return factory;
 });
